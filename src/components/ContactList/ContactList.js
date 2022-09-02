3ae628e7454d5d15bbcee636bbb2f8b375a832de
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-02 19:50:01 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,32 +1,29 @@
 import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
 import ContactItem from '../contactItem/contactItem';
-function ContactList({ contacts, onDeleteContact }) {
+function ContactList({ contacts, onDel }) {
+  // console.log('contacts', contacts);
+  // console.log('onDeleteContact', onDeleteContact);
   return (
     <ul className={css.list}>
       {contacts.map(({ id, name, number }) => (
         <li className={css.item} key={id}>
-          <ContactItem
-            user={name}
-            tel={number}
-            onDel={onDeleteContact}
-            delId={id}
-          />
+          <ContactItem user={name} tel={number} onDel={onDel} delId={id} />
         </li>
       ))}
     </ul>
   );
 }
 
-// ContactList.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.string.isRequired,
-//       name: PropTypes.string.isRequired,
-//       number: PropTypes.string.isRequired,
-//     })
-//   ),
-//   onDeleteContact: PropTypes.func.isRequired,
-// };
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDel: PropTypes.func.isRequired,
+};
 
 export default ContactList;
