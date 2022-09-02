7727d7532bbb61f4ae5952a64880f28a104c24b2
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-02 19:50:18 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,41 +10,20 @@ import ContactList from './components/ContactList/ContactList';
 import css from './App.module.css';
 
 function App() {
-  // state = {
-  //   contacts: [],
-  //   filter: '',
-  // };
-
   const [contacts, setContacts] = useState(loadFromStorage('phonebook'));
   const [filter, setFilter] = useState('');
-  console.log('contacts1', contacts);
-  console.log('loadFromStorage1', loadFromStorage('phonebook'));
-  console.log('contacts2', contacts);
 
-  let stor = loadFromStorage('phonebook');
-  console.log('stor', stor);
-  useEffect(contacts => {
-    if (
-      contacts !== null &&
-      contacts !== '' &&
-      contacts !== undefined &&
-      contacts !== loadFromStorage('phonebook')
-    ) {
-      console.log('contacts3', contacts);
-      console.log('loadFromStorage2', loadFromStorage('phonebook'));
-      saveInStorage('phonebook', contacts);
-    }
-  }, []);
   useEffect(() => {
-    console.log('contacts3', contacts);
+    saveInStorage('phonebook', contacts);
   }, [contacts]);
+
   const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
     };
-    // const { contacts } = this.state;
+
     if (
       contacts.find(
         contact => contact.name.toLowerCase() === name.toLowerCase()
@@ -55,33 +34,28 @@ function App() {
       alert(`Number ${number} is already in contacts.`);
     } else {
       setContacts([...contacts, contact]);
-      console.log('contacts5', contacts);
-      // saveInStorage('phonebook', contacts);
     }
   };
 
-  // const deleteContact = contactId => {
-  //   this.setState(({ contacts }) => ({
-  //     contacts: contacts.filter(contact => contact.id !== contactId),
-  //   }));
-  // };
-
   const changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
+    setFilter(e.currentTarget.value);
   };
 
-  const getVisibleContacts = () => {
-    // const { contacts, filter } = state;
+  const getFilterContacts = () => {
     const normalizedFilter = filter.toLowerCase();
-
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  // render() {
-  //   const { filter, contacts } = this.state;
-  //   const visibleContacts = this.getVisibleContacts();
+  const deleteContact = contactId => {
+    const del = contacts.filter(contact => contact.id !== contactId);
+    console.log('del', del);
+    setContacts(del);
+  };
+
+  const filteredContacts = getFilterContacts();
+
   return (
     <div className={css.container}>
       <h1>Phonebook</h1>
@@ -89,16 +63,12 @@ function App() {
       <h2>Contacts</h2>
       {contacts.length > 1 && <Filter value={filter} onChange={changeFilter} />}
       {contacts.length > 0 ? (
-        <ContactList
-          contacts={getVisibleContacts}
-          onDeleteContact={deleteContact}
-        />
+        <ContactList contacts={filteredContacts} onDel={deleteContact} />
       ) : (
         <p>Your phonebook is empty. Please add contact.</p>
       )}
     </div>
   );
 }
-// }
 
 export default App;
