d15ef97179dc55b12e75a7189fa3f2c44e2d556d
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-02 14:45:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,31 @@ function App() {
   //   contacts: [],
   //   filter: '',
   // };
-  const[contacts, setContacts]=useState([]);
-	const[filter, setFilter]=useState('');
-useEffect(()=>{setContacts(contacts = loadFromStorage('phonebook'))},[]);
-  // componentDidMount() {
-  //   const contacts = loadFromStorage('phonebook');
-  //   if (contacts) {
-  //     this.setState({ contacts: contacts });
-  //   }
-  //}
-	useEffect((contacts)=>{
-		if (contacts !== loadFromStorage('phonebook'))
-		{saveInStorage('phonebook', contacts)}},[contacts]);
-  // componentDidUpdate(prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     saveInStorage('phonebook', this.state.contacts);
-  //   }
-  // }
 
-  addContact = ({ name, number }) => {
+  const [contacts, setContacts] = useState(loadFromStorage('phonebook'));
+  const [filter, setFilter] = useState('');
+  console.log('contacts1', contacts);
+  console.log('loadFromStorage1', loadFromStorage('phonebook'));
+  console.log('contacts2', contacts);
+
+  let stor = loadFromStorage('phonebook');
+  console.log('stor', stor);
+  useEffect(contacts => {
+    if (
+      contacts !== null &&
+      contacts !== '' &&
+      contacts !== undefined &&
+      contacts !== loadFromStorage('phonebook')
+    ) {
+      console.log('contacts3', contacts);
+      console.log('loadFromStorage2', loadFromStorage('phonebook'));
+      saveInStorage('phonebook', contacts);
+    }
+  }, []);
+  useEffect(() => {
+    console.log('contacts3', contacts);
+  }, [contacts]);
+  const addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
@@ -48,24 +54,24 @@ useEffect(()=>{setContacts(contacts = loadFromStorage('phonebook'))},[]);
     } else if (contacts.find(contact => contact.number === number)) {
       alert(`Number ${number} is already in contacts.`);
     } else {
-      this.setState(({ contacts }) => ({
-        contacts: [contact, ...contacts],
-      }));
+      setContacts([...contacts, contact]);
+      console.log('contacts5', contacts);
+      // saveInStorage('phonebook', contacts);
     }
   };
 
-  deleteContact = contactId => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
+  // const deleteContact = contactId => {
+  //   this.setState(({ contacts }) => ({
+  //     contacts: contacts.filter(contact => contact.id !== contactId),
+  //   }));
+  // };
 
-  changeFilter = e => {
+  const changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
 
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
+  const getVisibleContacts = () => {
+    // const { contacts, filter } = state;
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -73,28 +79,26 @@ useEffect(()=>{setContacts(contacts = loadFromStorage('phonebook'))},[]);
     );
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <div className={css.container}>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-        <h2>Contacts</h2>
-        {contacts.length > 1 && (
-          <Filter value={filter} onChange={this.changeFilter} />
-        )}
-        {contacts.length > 0 ? (
-          <ContactList
-            contacts={visibleContacts}
-            onDeleteContact={this.deleteContact}
-          />
-        ) : (
-          <p>Your phonebook is empty. Please add contact.</p>
-        )}
-      </div>
-    );
-  }
+  // render() {
+  //   const { filter, contacts } = this.state;
+  //   const visibleContacts = this.getVisibleContacts();
+  return (
+    <div className={css.container}>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={addContact} />
+      <h2>Contacts</h2>
+      {contacts.length > 1 && <Filter value={filter} onChange={changeFilter} />}
+      {contacts.length > 0 ? (
+        <ContactList
+          contacts={getVisibleContacts}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <p>Your phonebook is empty. Please add contact.</p>
+      )}
+    </div>
+  );
 }
+// }
 
 export default App;
