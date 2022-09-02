85e6a16cd49b0d1c09030fb129112026dd32ee13
TkachAn <4ndrey.tkachenko@gmail.com>
2022-09-02 14:45:15 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -18,15 +18,15 @@ function ContactList({ contacts, onDeleteContact }) {
   );
 }
 
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ),
-  onDeleteContact: PropTypes.func.isRequired,
-};
+// ContactList.propTypes = {
+//   contacts: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     })
+//   ),
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
 
 export default ContactList;
